6d4d2c236e7da5147cbbcfa6e9ba7f12323b0910
stefan.duadsko <stefan.dudasko@triad.sk>
2020-10-21 17:19:04 +0200

    init

---
+++ Node.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace artificial_intelligence_8_hlavolam
+{
+    // State: 2d arr
+    // Heuristics: Number of nodes PATHS which do not correspond with their position (4+4+3+2+...n(pos)) n = 9
+    // We do represent blank as 0 for better visualization
+
+    // Honorable mentions: we might consider adjusting algorithm to not make redundant,
+    // possibly infinite loops in case of 2 step same result situation: (1, 2, 3) -> (1, 3, 2) -> (1, 2, 3) deadlock
+
+    // Algorithm:
+    // 1. We are creating nodes and putting them into a queue
+    public class Node
+    {
+        string[] _operators = {
+            "up", "right", "down", "left",
+        };
+
+        int [,] current_state = new int [3, 3] { // Current stav
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 0 },
+        };
+
+        int[,] satisfiable_state = new int[3, 3] { // Current stav
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 0, 8 },
+        };
+
+        Node parent_node = null; // Parent node
+        string _operator = null; // right, left, ...
+        int depth = -1; // Depth
+        int cost = -1; // Current trace cost
+
+        public Node(int[,] startingState = null, int[,] satisfiableState = null)
+        {
+            //this.printMatrix(this.current_state);
+            this.printMatrix(this.satisfiable_state);
+        }
+
+        /**
+         * https://stackoverflow.com/a/12827010/6525417
+         */
+        private void printMatrix(int [,] rawNodes)
+        {
+            int rowLength = rawNodes.GetLength(0);
+            int colLength = rawNodes.GetLength(1);
+            string arrayString = "";
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    arrayString += string.Format("{0} ", rawNodes[i, j]);
+                }
+                arrayString += System.Environment.NewLine + System.Environment.NewLine;
+            }
+
+            Console.WriteLine(arrayString);
+        }
+    }
+
+}
+ 
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace artificial_intelligence_8_hlavolam
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            new Node();
+        }
+    }
+}
